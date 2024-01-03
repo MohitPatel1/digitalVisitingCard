18e6b7821704f3231d889770cc88ca4e2c68610f
Mohit <mohit.patel1966@gmail.com>
2024-01-03 14:11:40 +0530

    mail links working

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,33 @@
 import React from 'react';
 import './App.css';
 
+function Image({src, size}){
+	const height = (size == 'small') ? 24 : 74;
+	return (
+		<img className='flex justify-between mr-2' src={src} height={height} width={height} />
+	)
+}
+
+function Link({link, type=''}){	
+	let url = ''
+	console.log(type);
+	switch (type) {
+		case 'mail':
+			url = 'mailto:' + link;
+			break;
+		default:
+			url = 'https://' + link;
+			break;
+	}	
+	if(type == 'mail'){
+		
+	}else{
+		
+	}
+	return <a className="value" href={`${url}`} target='_blank'>{link}</a>
+		 
+}
+
 function ProfilePicture({ src }) {
   return <img className="profile-picture" src={src} alt="Profile" />;
 }
@@ -10,25 +37,39 @@ function WallPicture({ src }) {
 	return <img className="wall-picture" src={src}  alt="Wall-Image"/>;
   }
 
-function ContactInfo({ icon, value }) {
+function ContactInfo({ icon, value, type }) {
+	console.log(type)
   return (
-    <div className="contact-info">
-      <img src={icon}/>
-      <span className="value">{value}</span>
+    <div className="contact-info">		
+      <Image src={icon} size='small' />
+	  {console.log(value)}Link
+      <Link className="value" link={value} type={type}>{value}</Link>
     </div>
   );
+}
+
+function Socials({socials}){
+	console.log(socials)
+	return(
+		<div className='flex gap-1 space-between p-1'>
+			{socials.map(({icon,value}) =>  ( 	
+				<Image src={icon} href={value} key={icon} size='regular'/>
+			))}
+		</div>
+	)
 }
 
 function Footer({ address, website }) {
   return (
     <div>
-      <div className="flex justify-center items-center gap-1"> 
-	  	<img src='/icons/location.svg'/>
+      <div className="flex space-around gap-1"> 
+	  	<Image src='/icons/location.png' size='small'/>
 		<span className="value text-center">{address}</span>
 	  </div>
 	  <div className="flex justify-center items-center gap-1"> 
-	  	<img src='/icons/website.svg'/>
-		<span className="value">{website}</span>
+	  	<Image src='/icons/website.png' size='small'/>
+		{console.log(`${website}`)}
+		<Link className="value" link={website} type=''>{website}</Link>
 	  </div>
     </div>
   );
@@ -45,9 +86,10 @@ function Card({ user }) {
 		<h1>{user.name}</h1>
 		<h4>{user.title}</h4>
 		{user.contactDetails.map((detail) => (
-			<ContactInfo key={detail.icon} icon={detail.icon} value={detail.value} />
+			<ContactInfo key={detail.icon} icon={detail.icon} type={detail.type} value={detail.value} />
 		))}
 		<Footer address={user.address} website={user.website} />
+		<Socials socials={user.socials}/>	
 	  </div>
     </div>
   );
@@ -60,12 +102,19 @@ function App() {
 	wallPicture:'/smartAgent512x512.png',
     profilePicture: '/krishna.jpg',
     contactDetails: [
-      { icon: '/icons/phone.svg', value: '+91 7575806994' },
-	  { icon: '/icons/telephone.svg', value: '079 26920886' },
-      { icon: '/icons/website.svg', value: 'hello@smartagent.one' },
+      { type:'phone', icon: '/icons/phone.png', value: '+91 7575806994' },
+	  { type:'landLine', icon: '/icons/landline.png', value: '079 26920886' },
+      { type:'mail', icon: '/icons/gmail.png', value: 'hello@smartagent.one' },
     ],
+	socials: [
+	  { icon: '/icons/whatsapp.png', value: 'wa.me/7575806994' },
+	  { icon: '/icons/twitter.png', value: 'https://x.com/krishnaa404' },
+	  { icon: '/icons/linkedin.png', value: 'linkedin.com/in/krishna-teziapp' },
+	  { icon: '/icons/instagram.png', value: 'wa.me/7575806994' },
+
+	],
     address: '290M New Cloth Market, Sarangpur Ahmedabad, 380002',
-    website: 'www.smartagent.one',
+    website: 'smartagent.one',
   };
 
   return (
